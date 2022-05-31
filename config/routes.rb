d17534bb7d8760a892460dc028b6771e6d288404
Ludovic Boisgirard <ludovic.boisgirard@gmail.com>
2022-05-31 18:13:58 +0200

    avec routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: 'pages#home'
+
+  resources :companies, only: %i[show edit update] do
+    resources :projects, except: %i[index delete] do
+      resources :tasks, only: %i[new edit update]
+    end
+  end
+  resources :candidates, except: :delete
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
