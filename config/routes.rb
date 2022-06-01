e6fc7a5a4094bfa0d0e8064470890a481861086f
Ludovic Boisgirard <ludovic.boisgirard@gmail.com>
2022-06-01 16:05:33 +0200

    candidates v2

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :companies, only: %i[show edit update] do
-    resources :projects, except: %i[index destroy] do
-      resources :tasks, only: %i[new]
+    resources :projects, except: %i[index destroy update edit] do
+      resources :tasks, only: %i[create]
     end
   end
   resources :candidates, except: :destroy
   resources :tasks, only: %i[update]
+  resources :projects, only: %i[update edit]
 
-  get 'accept', to: 'tasks#accept'
+  # get 'accept', to: 'tasks#accept'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
