542bc09b08bc0c52ec3893f16380b4030685e439
Ludovic Boisgirard <ludovic.boisgirard@gmail.com>
2022-06-08 21:26:34 +0200

    css et boutons

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,24 +12,7 @@ class TasksController < ApplicationController
     @task.save!
     @chatroom = Chatroom.create(name: @project.name, task: @task)
     redirect_to company_project_path(@company, @project)
-
   end
-
-  # def accept
-  #   task = Task.find(params['task_id'])
-  #   task.status = "Accepted"
-  #   task.save
-  #   redirect_to (candidate_path(task.candidate))
-  # end
-
-  # def update
-  #   @task.update(task_params)
-  #   if current_user.corporate
-  #     redirect_to company_path(current_user)
-  #   else
-  #     redirect_to candidate_path(current_user)
-  #   end
-  # end
 
   def accept
     @task.status = "Accepted"
